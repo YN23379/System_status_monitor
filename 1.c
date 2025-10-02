7382fed498e5f24e81a1e2b2f06c6b0dfb2e5aef
YN23379 <yn23379@outlook.com>
2025-10-02 23:32:46 +0800

    cpu_usage_monitor

---
+++ 1.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+#define CPU_LINE 100
+char a[200];
+int get_cpu_monitor()  
+{                         
+    FILE* fp;
+    fp = fopen("/proc/stat", "r");
+    char file[CPU_LINE];
+    if (!fp)
+    {
+        perror("Cannot open : ");
+        return -1;
+    }
+
+    else
+    {
+        while (fgets(file, CPU_LINE, fp) != NULL)
+        {
+            char* p = strstr(file, "cpu");
+            if (p == NULL)
+                continue;
+            if (strncmp(file, "cpu", 3) == 0 && file[3] == ' ')
+                strcpy(a, file);
+            return 1;
+        }
+    }
+
+}
+
+void printf_cpu_monitor()
+{
+    if (get_cpu_monitor())
+    {
+        puts(a);         
+    }
+}
+void printf_cpu_usage()
+{
+    long num[10] = { 0 };
+    char jug[2] = " ";
+    char* token = strtok(a, jug);
+    int i = 0;
+    while (token != NULL && i < 10)
+    {
+        token = strtok(NULL, jug);
+        if (token != NULL)
+        {
+            num[i] = strtol(token, NULL, 10);  
+            i++;
+        }
+    }
+    static int total_old = 0, idle_old = 0;
+    double usage;
+    int total_new = 0, idle_new = 0;
+    int total_delta = 0, idle_delta = 0;
+    total_old = total_new;
+    idle_old = idle_new;
+    for (i = 0; i < 10; i++)
+    {
+        total_new = total_new + num[i];
+        if (i == 3 || i == 4)
+            idle_new += num[i];
+    }
+    total_delta = total_new - total_old;
+    idle_delta = idle_new - idle_old;
+
+    usage = 1 - (double)idle_delta / total_delta;
+
+    printf("CPU usage is:%.2lf%%\n", usage * 100);
+}
+void printf_cpu()
+{
+    printf_cpu_monitor();
+    printf_cpu_usage();
+}
+int main()
+{
+ while(1)
+  {
+  	printf("-----System Status-----\n");
+	printf_cpu();
+    printf("-----System Status-----\n");
+	
+	
+	sleep(5);
+  }
+  return 0;
+}
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include"Lx_grepMemavailable.h"
 #include"Lx_Loadaverage.h"
+#include"cpu_monitor.h"
 #include <stdlib.h>
 #include <string.h>
 #include<unistd.h>
@@ -13,7 +14,11 @@ int main()
   	printf("-----System Status-----\n");
 	print_load_info();
 	print_memory_info();
+	printf_cpu();
+    
     printf("-----System Status-----\n");
+	
+	
 	sleep(5);
   }
   return 0;
